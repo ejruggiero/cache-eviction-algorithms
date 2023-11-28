71a9414c8a9835f88e376c53dd96fed856e75346
Elizabeth Ruggiero <ejr2@princeton.edu>
2023-11-28 13:46:13 -0500

    added images to help popup

--- src/components/Help/Help.js
+++ src/components/Help/Help.js
@@ -2,6 +2,12 @@ import React from 'react';
 import 'react-responsive-modal/styles.css';
 import './Help.css';
 import { Modal } from 'react-responsive-modal';
+import dropdown from '../../images/dropdown.png';
+import cache from '../../images/cache.png';
+import dataElem from '../../images/data_elem.png';
+import incomingElem from '../../images/incoming_elem.png';
+import score from '../../images/score.png';
+import help from '../../images/help.png';
 
 export const Help = () => {
   const [open, setOpen] = React.useState(true);
@@ -10,14 +16,14 @@ export const Help = () => {
     <div>
     <h1>Welcome!</h1>
     <p>Hello! Here you will learn three important cache eviction algorithms: first in first out (FIFO),
-     least recently used (LRU), and least frequently used (LFU). Change the algorithm you wish to practice by toggling the dropdown at the top of the screen.</p>
-    <p>The cache is represented by a gray rectangle near the center of the screen. Data elements are represented as yellow circles with emojis 
-     representing their data. Because the cache has a capacity of 5 data elements, there will be 5 data elements added to the cache. 
-     The data element to the left of the cache represents an incoming data element that is to be added to the cache. Click on the data element 
-     that should be removed. 
-     Your score can be found at the top right corner of the screen. If you select the correct element to remove from the cache, then you will 
-     gain a point.</p>
-     <p>You can reopen this popup at any time by clicking on the Help button.</p>
+     least recently used (LRU), and least frequently used (LFU).</p>
+    <p>Change the algorithm you wish to practice by toggling the dropdown <img src={dropdown} alt="Dropdown" width={250} height={40}/> at the top of the screen.</p>
+    <p>The cache is represented by a gray rectangle near the center of the screen. <img src={cache} alt="Cache" width={100} height={25}/> It has a capacity of 5 data elements. Data elements are represented as yellow circles containing emojis 
+     which represent their data. <img src={dataElem} alt="Data Element" width={50} height={50}/></p>
+     <p>The data element to the left of the cache represents an incoming data element <img src={incomingElem} alt="Incoming Data Element" width={50} height={50}/> that we want to add to the cache. <b>If it matches an element in the cache,</b> then click on that matching element in the cache. This is known as a <b>hit</b> -- the data is successfully retrieved from the cache. <b>If the incoming element does not match any element in the cache,</b> then click the element that should be evicted from the cache based on the eviction algorithm you have chosen.</p>
+     <p>Your score can be found at the top right corner of the screen. <img src={score} alt="Score" width={150} height={38}/> If you select the correct element to remove from the cache, then you will 
+     gain 1 point.</p>
+     <p>You can reopen this popup at any time by clicking on the Help button. <img src={help} alt="Help" width={70} height={50}/></p>
      </div>
   );
 
